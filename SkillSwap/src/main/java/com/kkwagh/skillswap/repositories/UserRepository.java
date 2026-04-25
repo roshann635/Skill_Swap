19534cd9dfa07e4fb88a77d58be864aa658abd94
roshann635 <roshanjadhav4385@gmail.com>
2026-04-25 10:39:11 +0530

    fix: add missing List import to UserRepository

--- SkillSwap/src/main/java/com/kkwagh/skillswap/repositories/UserRepository.java
+++ SkillSwap/src/main/java/com/kkwagh/skillswap/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.kkwagh.skillswap.repositories;
 
 import com.kkwagh.skillswap.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
